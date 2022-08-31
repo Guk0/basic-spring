2c29f9f2b91319c5a47e4497c15a34660edb51f7
guk0 <abcgy39@naver.com>
2022-08-31 16:18:52 +0900

    initial commit / before using spring bin

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+        // 생성자를 통해 추상화(interface)에만 의존하게함.
+        // -> 생성자 주입
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,31 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+
+    private final DiscountPolicy discountPolicy;
+    // 누군가가 discountPolicy의 구현객체를 대신 생성하고 주입해줘야함.
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice); // db에 따로 저장하지 않는다.
+    }
+}
